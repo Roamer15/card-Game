c3112fff721093dd519eb9144c4193c4ddf2df5d
Beleke Ian <ianseeksbeta@gmail.com>
2024-12-24 18:12:42 +0100

    Main div seperation

---
+++ js/index.js
@@ -0,0 +1,8 @@
+import 'css/style.css'
+
+document.querySelector('#game-container').innerHTML = `
+   <div class="navbar"></div>
+   <div class="body-options">
+      <button></button>
+   </div>
+`
