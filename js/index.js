e6687cf4fffd3b68c1f7d6c93e375442a10b17fa
Beleke Ian <ianseeksbeta@gmail.com>
2024-12-25 20:27:25 +0100

    Landing page done

--- js/index.js
+++ js/index.js
@@ -1,10 +1,5 @@
 import 'css/style.css'
 
 document.querySelector('#game-container').innerHTML = `
-   <div class="navbar"></div>
-   <div class="body-options">
-      <button>Start Game</button>
-      <button>How to Play</button>
-      <button>Sound</button>
-   </div>
+
 `
