f0222cc3352dbc4f2046ab3039415f022bf22c23
Roamer15 <ianseeksbeta@gmail.com>
2025-01-13 09:09:56 +0100

    Linters fixed

--- js/index.js
+++ js/index.js
@@ -2,15 +2,13 @@ const play = document.querySelector('.btn')
 const learn = document.querySelector('.btn-mobile')
 
 play.addEventListener('click', () => {
-
   setTimeout(() => {
     window.location.href = 'game.html'
-    },2000)
+  }, 2000)
 })
 
 learn.addEventListener('click', () => {
   setTimeout(() => {
- window.location.href = 'about.html'
-  },2000)
- 
+    window.location.href = 'about.html'
+  }, 2000)
 })
